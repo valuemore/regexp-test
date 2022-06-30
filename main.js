609843e890d91596f2228d5083dac6ea7cef0805
valuemore <valuemore@naver.com>
2022-06-30 16:53:21 +0900

    Regular Expression

---
+++ main.js
@@ -0,0 +1,14 @@
+const str = `
+010-6723-4401
+thesecond@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+hyxp
+http://localhost:2000
+동해물과 백두산이 마르고 닳도록
+`
+const h = `  the hello  world   !
+
+`
+console.log(h.replace(/\s/g, ''))
